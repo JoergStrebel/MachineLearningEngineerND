da48d9321963ce302ef99ddb37899bf8f6348514
Jörg Strebel <joerg.strebel@gmail.com>
2019-08-02 22:00:57 +0200

    Added conda environment setup

---
+++ customer_segments/condaenv.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+conda create -n customersegments
+conda install --name customersegments --file requirements.txt
+
---
+++ customer_segments/start_jupyter.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+source activate customersegments
+jupyter notebook --ip=127.0.0.1
