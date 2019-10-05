4729cd012064a2d72a5d3eac633b4faef0a035dc
Jörg Strebel <joerg.strebel@gmail.com>
2019-10-05 18:35:34 +0200

    Tweaked parameters

--- rl_quadcopter_2/task.py
+++ rl_quadcopter_2/task.py
@@ -1,10 +1,12 @@
 import numpy as np
 from physics_sim import PhysicsSim
 
+
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+
+    def __init__(self, init_pose=None, init_velocities=None,
+                 init_angle_velocities=None, runtime=5., target_pos=None):
         """Initialize a Task object.
         Params
         ======
@@ -15,7 +17,7 @@ class Task():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
         self.state_size = self.action_repeat * 6
@@ -24,39 +26,44 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         """
         Uses current pose of sim to return reward.
-        The function implements a reward function based on the Euclidean distance (L2 norm),
-        however it is mapped to the interval [-1.0 ... 1.0] using tanh() to make it easier to learn.
         """
+        # This part of the reward function is based on the Euclidean distance (L2 norm)
         l2norm = np.sqrt(np.sum(np.square(self.sim.pose[:3] - self.target_pos)))
-        #reward_distance = -2.0 * np.tanh(0.1 * l2norm) + 1.0
-        reward_distance = -l2norm/10
-        angles=[]
-        for index in range(len(self.sim.pose[3:])):
-            x=self.sim.pose[index]
-            if x>np.pi:
-                angles.append(x-2*np.pi)
-            else:
-                 angles.append(x)
-        npangles = np.array(angles)
-        #reward_tilt=np.amin(-2.0*np.tanh(np.abs(4.0*npangles))+1) #penalizes poses that are too tilted
-        reward_tilt=-np.sum(np.abs(npangles))
+        # reward_distance = -2.0 * np.tanh(0.1 * l2norm) + 1.0
+        reward_distance = -l2norm/5
 
-        basic_reward=1.0 #you get at least 1 if the quadcopter manages to stay in the air
+        # This part of the reward function is based on the tilt of the quadcopter
+        #angles = []
+        #for index in range(len(self.sim.pose[3:])):
+         #   x = self.sim.pose[index]
+         #   if x > np.pi:
+         #       angles.append(x - 2 * np.pi)
+         #   else:
+         #       angles.append(x)
+        #npangles = np.array(angles)
+        # reward_tilt=np.amin(-2.0*np.tanh(np.abs(4.0*npangles))+1) #penalizes poses that are too tilted
+        reward_tilt = -np.sum(self.sim.pose[3:])
 
-        return basic_reward+(reward_tilt+reward_distance)/2
+        # This part of the reward function is based on the angular velocity of the quadcopter
+        #reward_angv = -np.sum(np.abs(self.sim.angular_v))
+
+        # you get at least a basic reward if the quadcopter manages to stay in the air
+        basic_reward = 2.0
+
+        return basic_reward + (reward_tilt + reward_distance ) / 2
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
         pose_all = []
         for _ in range(self.action_repeat):
-            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            done = self.sim.next_timestep(rotor_speeds)  # update the sim pose and velocities
+            reward += self.get_reward()
             pose_all.append(self.sim.pose)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
@@ -64,5 +71,5 @@ class Task():
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([self.sim.pose] * self.action_repeat)
         return state
