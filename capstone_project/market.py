2ca2784480136592656c042da4c0a4043aaa60c3
Jörg Strebel <joerg.strebel@bmw.de>
2019-11-19 21:41:16 +0100

    defined agent structure

--- capstone_project/market.py
+++ capstone_project/market.py
@@ -1,1 +1,22 @@
- 
+import numpy as np
+import pandas as pd
+
+class Market():
+    def __init__(self, dfdata):
+        self.startdate = None
+        self.enddate = None
+        # init test /validation data
+        self.reset(dfdata)
+
+    def reset(self, dfdata):
+        self.date = None  # set the minimum date from the dfdata
+        self.stockprices = None # what is the current market price
+        self.marketdata = dfdata
+        self.done = False
+
+    def next_timestep(self, rotor_speeds):
+        self.stockprices = None
+        self.date += self.dt
+        if self.date > self.enddate:
+            self.done = True
+        return self.done
--- capstone_project/task.py
+++ capstone_project/task.py
@@ -1,22 +1,18 @@
 import numpy as np
-from physics_sim import PhysicsSim
+from  market import Market
+import pandas as pd
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None,
-                 init_angle_velocities=None, runtime=5., target_pos=None):
+    def __init__(self, oMarkt: Market):
         """Initialize a Task object.
         Params
         ======
-            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
-            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
-            init_angle_velocities: initial radians/second for each of the three Euler angles
-            runtime: time limit for each episode
-            target_pos: target/goal (x,y,z) position for the agent
+
         """
-        # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
-        self.action_repeat = 3
+        # Market
+        self.sim = oMarkt
+        self.action_repeat = 1
 
         self.state_size = self.action_repeat * 6
         self.action_low = 0
@@ -24,7 +20,7 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+        self.target_pos = None
 
     def get_reward(self):
         """
